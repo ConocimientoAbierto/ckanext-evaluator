6e2043ec40fb4b42656ca8cc774ea3d5581f0067
manujas <arias.emanuel@gmail.com>
2016-11-21 19:21:31 -0300

    new tab for the extension is working and pasing all data of the org

--- ckanext/evaluator/controller.py
+++ ckanext/evaluator/controller.py
@@ -1,20 +1,33 @@
-import ckan.lib.base as base
-import ckan.logic as logic
-# import ckan.plugins as p
-import ckan.plugins.toolkit as toolkit
+from ckan.lib.base import render, c, abort
+from ckan.controllers.group import GroupController
+from ckan import model, logic
+import ckan.plugins as plugins
+# import ckan.plugins.toolkit.c as c
 
-class EvaluatorController(base.BaseController):
+# class EvaluatorController(base.BaseController):
+class EvaluatorController(GroupController):
 
     def evaluation(self, id):
-        ## TODO esto debería funcionar pero no parece que no llega al controladador
 
         try:
-            toolkit.c.group_dict = toolkit.get_action('organization_show')(
-                None, {'id': id}
-            )
-        except logic.NotFound:
-            base.abort(404, _('Resource not found'))
-        except logic.NotAuthorized:
-            base.abort(401, _('Unauthorized to edit this resource'))
+            # toolkit.c.group_dict = toolkit.get_action('organization_show')(
+            #     None, {'id': id}
+            # )
 
-        return base.render('organization/evaluation.html')
+            ## Get data from DB
+            self.group_type = 'organization'
+            context = {'model': model, 'session': model.Session,
+                       'user': c.user or c.author,
+                       'schema': self._db_to_form_schema(group_type=self.group_type),
+                       'for_view': True, 'extras_as_string': True}
+            data_dict = {'id': id}
+
+            c.group_dict = self._action('organization_show')(context, data_dict)
+            c.group = context['group']
+        except logic.NotFound:
+            # abort(404, _('Resource not found'))
+            abort(404, 'Esto es un 404 desde mi controller')
+        except logic.NotAuthorized:
+            abort(401, 'Esto es un 401 desde mi controller')
+
+        return render('organization/evaluation.html')
--- ckanext/evaluator/plugin.py
+++ ckanext/evaluator/plugin.py
@@ -1,32 +1,24 @@
+import routes.mapper
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-import routes.mapper
 
 
 class EvaluatorPlugin(plugins.SingletonPlugin):
-
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IRoutes)
 
-    # IConfigurer
+    def update_config(self, config):
 
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_template_directory(config, 'templates')
+        toolkit.add_public_directory(config, 'public')
         toolkit.add_resource('fanstatic', 'evaluator')
 
-    # IRoutes
+    def before_map(self, route_map):
+        with routes.mapper.SubMapper(route_map,
+                controller='ckanext.evaluator.controller:EvaluatorController') as m:
+            m.connect('organization_evaluation', '/organization/evaluation/{id}', action='evaluation', ckan_icon='bar-chart')
 
-    def before_map(self, map):
-        controller = 'ckanext.evaluator.controller:EvaluatorController'
+        return route_map
 
-        with routes.mapper.SubMapper(map, controller=controller) as m:
-            m.connect('organization_evaluation',
-                      'organization/evaluation/{id}',
-                      action='evaluation',
-                      ckan_icon='sitemap')
-
-        return map
-
-    def after_map(self, map):
-        return map
+    def after_map(self, route_map):
+        return route_map

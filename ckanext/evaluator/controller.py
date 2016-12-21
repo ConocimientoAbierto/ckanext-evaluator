8096f75458f43a31dce15fd0567ac3aebf627f7f
manujas <arias.emanuel@gmail.com>
2016-12-21 17:40:27 -0300

    adding totalizator and inserting on view
    Just adding the sum of points on the report of metadata and inserting
    this total on the view fo evaluation

--- ckanext/evaluator/controller.py
+++ ckanext/evaluator/controller.py
@@ -54,39 +54,42 @@ class EvaluatorController(GroupController):
     def _matadata_evaluation(self, pkg_dict):
 
         reporte = {
-            'explicacion': {
-                'descripcion': 'Explicacion del dataset',
-                'puntaje': 0,
-                'motivo': 'No posee descripcion y/o explicacion.'
-            },
-            'responsable': {
-                'descripcion': 'Responsable informado',
-                'puntaje': 0,
-                'motivo': 'No posee autor y/o responsable identificados con su nombre y mail.'
-            },
-            'frec_act_declarada': {
-                'descripcion': 'Fecha de actualizaci0n informada',
-                'puntaje': 0,
-                'motivo': 'No posee informada la frecuencia de actualizacion.'
-            },
-            'frec_act_efectiva': {
-                'descripcion': 'Fecha de actualizacion respetada',
-                'puntaje': 0,
-                'motivo': 'La frecuencia de actualizacion no coincide con la informada o la misma no esta informada.'
-            },
-            'formato': {
-                'descripcion': 'Formato de los archivos',
-                'puntaje': 0,
-                'motivo': 'El formato de cada archivo no facilita su reutilizacion.'
-            },
+            'total': 0,
+            'categorias': {
+                'explicacion': {
+                    'descripcion': 'Explicacion del dataset',
+                    'puntaje': 0,
+                    'motivo': 'No posee descripcion y/o explicacion.'
+                },
+                'responsable': {
+                    'descripcion': 'Responsable informado',
+                    'puntaje': 0,
+                    'motivo': 'No posee autor y/o responsable identificados con su nombre y mail.'
+                },
+                'frec_act_declarada': {
+                    'descripcion': 'Fecha de actualizaci0n informada',
+                    'puntaje': 0,
+                    'motivo': 'No posee informada la frecuencia de actualizacion.'
+                },
+                'frec_act_efectiva': {
+                    'descripcion': 'Fecha de actualizacion respetada',
+                    'puntaje': 0,
+                    'motivo': 'La frecuencia de actualizacion no coincide con la informada o la misma no esta informada.'
+                },
+                'formato': {
+                    'descripcion': 'Formato de los archivos',
+                    'puntaje': 0,
+                    'motivo': 'El formato de cada archivo no facilita su reutilizacion.'
+                },
+            }
         }
         # TODO agregar 'licencia': {'puntaje': 0, 'motivo': 'La licencia informada no es de caracter abirto.'},
 
         # explicacion
         exp = pkg_dict['notes']
         if not exp or len(exp) > 150:
-            reporte['explicacion']['puntaje'] = 1
-            reporte['explicacion']['motivo'] = 'Posee una descripcion y/o explicacion'
+            reporte['categorias']['explicacion']['puntaje'] = 1
+            reporte['categorias']['explicacion']['motivo'] = 'Posee una descripcion y/o explicacion'
 
         # responsable
         res = pkg_dict['maintainer']
@@ -95,24 +98,24 @@ class EvaluatorController(GroupController):
         autor_mail = pkg_dict['author_email']
         email_regex = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
         if res or autor:
-            reporte['responsable']['puntaje'] = 0.5
-            reporte['responsable']['motivo'] = 'Autor y/o responsable identificados con nombre pero sin mail valido'
+            reporte['categorias']['responsable']['puntaje'] = 0.5
+            reporte['categorias']['responsable']['motivo'] = 'Autor y/o responsable identificados con nombre pero sin mail valido'
             if res and email_regex.match(res_mail) or autor and email_regex.match(autor_mail):
-                reporte['responsable']['puntaje'] = 1
-                reporte['responsable']['motivo'] = 'Autor y/o responsable identificados con nombre y mail valido'
+                reporte['categorias']['responsable']['puntaje'] = 1
+                reporte['categorias']['responsable']['motivo'] = 'Autor y/o responsable identificados con nombre y mail valido'
 
         # frecuencia de actualizacion declarada
         str_frec = 'frecuencia de actualizacion'.decode('utf-8')
         if str_frec in pkg_dict['extras']:
             frec_declarada = pkg_dict['extras']['str_frec']
-            reporte['frec_act_declarada']['puntaje'] = 0.5
-            reporte['frec_act_declarada']['motivo'] = 'Posee frecuencia de actualizacion informada pero la misma no es especifica.'
+            reporte['categorias']['frec_act_declarada']['puntaje'] = 0.5
+            reporte['categorias']['frec_act_declarada']['motivo'] = 'Posee frecuencia de actualizacion informada pero la misma no es especifica.'
             if frec_declarada != 'eventual':
-                reporte['frec_act_declarada']['puntaje'] = 1
-                reporte['frec_act_declarada']['motivo'] = 'Posee frecuencia de actualizacion informada.'
+                reporte['categorias']['frec_act_declarada']['puntaje'] = 1
+                reporte['categorias']['frec_act_declarada']['motivo'] = 'Posee frecuencia de actualizacion informada.'
 
         # frecuencia de actualizacion efectiva
-        if reporte['frec_act_declarada']['puntaje'] != 0:
+        if reporte['categorias']['frec_act_declarada']['puntaje'] != 0:
             for resource in pkg_dict['resources']:
                 now = dt.now()
                 last_modified = dt.strptime(resource['last_modified'], '%Y-%m-%dT%H:%M:%S')
@@ -120,11 +123,11 @@ class EvaluatorController(GroupController):
                 days_diff = time_diff.days
 
                 if days_diff < 15:
-                    reporte['frec_act_efectiva']['puntaje'] = 1
-                    reporte['frec_act_efectiva']['motivo'] = 'La frecuencia de actualizacion es menor o igual a 15 dias'
+                    reporte['categorias']['frec_act_efectiva']['puntaje'] = 1
+                    reporte['categorias']['frec_act_efectiva']['motivo'] = 'La frecuencia de actualizacion es menor o igual a 15 dias'
                 else:
-                    reporte['frec_act_efectiva']['puntaje'] = 0
-                    reporte['frec_act_efectiva']['motivo'] = 'La frecuencia de actualizacion es mayor a 15 dias'
+                    reporte['categorias']['frec_act_efectiva']['puntaje'] = 0
+                    reporte['categorias']['frec_act_efectiva']['motivo'] = 'La frecuencia de actualizacion es mayor a 15 dias'
 
         # licencia
         # TODO Verificar como es expresada, puede variar
@@ -133,7 +136,11 @@ class EvaluatorController(GroupController):
         formatos_abiertos = ['csv', 'json']
         for resource in pkg_dict['resources']:
             if resource['format'].lower() in formatos_abiertos:
-                reporte['formato']['puntaje'] = 1
-                reporte['formato']['motivo'] = 'El formato de cada archivo facilita su reutilizacion.'
+                reporte['categorias']['formato']['puntaje'] = 1
+                reporte['categorias']['formato']['motivo'] = 'El formato de cada archivo facilita su reutilizacion.'
+
+        # Total
+        for item in reporte['categorias']:
+            reporte['total'] = reporte['total'] + reporte['categorias'][item]['puntaje']
 
         return reporte
